c2d247c5707c1737eb354f167656dab06782f07c
SofHay <sofhay@live.se>
2017-05-06 17:20:48 +0200

    Navigation bar update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
@@ -23,13 +25,18 @@ public class Main extends Application {
     private GridPane grid;
     private Scene scene;
     private Button sendMessage;
+    private Button sendMessage1;
+    private Button sendMessage2;
+    private Button sendMessage3;
+    private Button sendMessage4;
+    private Button tmp1;
     private ImageView image;
 
 
     @Override
     public void start(Stage primaryStage){
         createGrid(primaryStage);
-        searchActions();
+
     }
 
     private void createGrid(Stage primaryStage){
@@ -38,33 +45,56 @@ public class Main extends Application {
         grid.setHgap(10);
         grid.setVgap(10);
         grid.setPadding(new Insets(5, 5, 5, 5));
-        image =new ImageView("images/2.png");
-        sendMessage = new Button("", image);
-        sendMessage.setContentDisplay(ContentDisplay.BOTTOM);
-        sendMessage.setStyle("-fx-background-color: -fx-outer-border, -fx-inner-border, -fx-body-color;");
 
-        HBox header = new HBox(8);
-        header.getChildren().addAll(sendMessage);
+        image =buttonIcon("images/2.png");
+        sendMessage = createButton(image);
+        searchActions(sendMessage,"Dela");
+
+        sendMessage1 = createButton(image);
+        sendMessage2 = createButton(image);
+        sendMessage3 = createButton(image);
+        sendMessage4 = createButton(image);
 
 
-        GridPane.setConstraints( header, 1, 0);
+
+        HBox header = new HBox();
+        header.getChildren().addAll(sendMessage, sendMessage1, sendMessage2, sendMessage3, sendMessage4);
+
+        GridPane.setConstraints( header, 20, 0);
+
+
         grid.getChildren().addAll( header);
         scene = new Scene(grid, 900,600);
-        scene.getStylesheets().add("style/Stylesheet.css");
+       // scene.getStylesheets().add("../style/Stylesheet.css");
+        scene.getStylesheets().add(getClass().getResource("../style/Stylesheet.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("BBB");
         primaryStage.show();
 
+
     }
-    private void searchActions(){
+    //Gives the button an id and image
+    private Button createButton(ImageView img){
+        Button button = new Button("", img);
+        button.setId("navButton");
+        return button;
+    }
+
+    //Gives the ImageView an id
+    private ImageView buttonIcon(String path){
+        ImageView imgV = new ImageView(path);
+        imgV.setId("navImg");
+        return imgV;
+    }
+
+    //Method add title on button if hovered
+    private void searchActions(Button button, String title){
         sendMessage.hoverProperty().addListener((ov, oldValue, newValue) -> {
             if (newValue) {
-                image.setStyle("-fx-opacity: 0.5;-fx-background-color: -fx-outer-border, -fx-inner-border, -fx-body-color;");
-                sendMessage.setText("Mejl");
-                sendMessage.setContentDisplay(ContentDisplay.CENTER);
+                button.setText(title);
+                button.setContentDisplay(ContentDisplay.CENTER);
             } else {
-                image.setStyle("-fx-background-color: -fx-outer-border, -fx-inner-border, -fx-body-color;");
-                sendMessage.setText("");
+                button.setText("");
             }
         });
 
