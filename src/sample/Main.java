09869c9550b454f5689a14d9461e7649e1fe4e8b
SofHay <sofhay@live.se>
2017-05-08 14:43:03 +0200

    New navigation bar

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,6 +13,9 @@ import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
 import javafx.scene.layout.HBox;
@@ -24,13 +27,14 @@ import java.awt.*;
 public class Main extends Application {
     private GridPane grid;
     private Scene scene;
-    private Button sendMessage;
-    private Button sendMessage1;
-    private Button sendMessage2;
-    private Button sendMessage3;
-    private Button sendMessage4;
-    private Button tmp1;
-    private ImageView image;
+    private Label edit;
+    private Label create;
+    private Label statistics;
+    private Label gradeTest;
+    private Label register;
+
+
+
 
 
     @Override
@@ -46,26 +50,22 @@ public class Main extends Application {
         grid.setVgap(10);
         grid.setPadding(new Insets(5, 5, 5, 5));
 
-        image =buttonIcon("images/2.png");
-        sendMessage = createButton(image);
-        searchActions(sendMessage,"Dela");
-
-        sendMessage1 = createButton(image);
-        sendMessage2 = createButton(image);
-        sendMessage3 = createButton(image);
-        sendMessage4 = createButton(image);
+        edit =createLabel("Redigera");
+        create=createLabel("Skapa test");
+        statistics=createLabel("Statistik");
+        gradeTest=createLabel("Rätta prov");
+        register=createLabel("registrera");
 
 
 
         HBox header = new HBox();
-        header.getChildren().addAll(sendMessage, sendMessage1, sendMessage2, sendMessage3, sendMessage4);
+        header.getChildren().addAll(edit,create,statistics,gradeTest,register);
 
         GridPane.setConstraints( header, 20, 0);
 
 
         grid.getChildren().addAll( header);
         scene = new Scene(grid, 900,600);
-       // scene.getStylesheets().add("../style/Stylesheet.css");
         scene.getStylesheets().add(getClass().getResource("../style/Stylesheet.css").toExternalForm());
         primaryStage.setScene(scene);
         primaryStage.setTitle("BBB");
@@ -73,32 +73,25 @@ public class Main extends Application {
 
 
     }
-    //Gives the button an id and image
-    private Button createButton(ImageView img){
-        Button button = new Button("", img);
-        button.setId("navButton");
-        return button;
+    //Gives the label an id and title
+        private Label createLabel(String title){
+        Label label = new Label(title);
+        label.setId("navLabel");
+        return label;
     }
 
-    //Gives the ImageView an id
-    private ImageView buttonIcon(String path){
-        ImageView imgV = new ImageView(path);
-        imgV.setId("navImg");
-        return imgV;
-    }
-
-    //Method add title on button if hovered
-    private void searchActions(Button button, String title){
-        sendMessage.hoverProperty().addListener((ov, oldValue, newValue) -> {
+    //Method change xxx of label when hovered
+    private void searchActions(Label label){
+            label.hoverProperty().addListener((ov, oldValue, newValue) -> {
             if (newValue) {
-                button.setText(title);
-                button.setContentDisplay(ContentDisplay.CENTER);
+
             } else {
-                button.setText("");
+
             }
         });
-
     }
+
+
     public static void main(String[] args) {
         launch(args);
     }
