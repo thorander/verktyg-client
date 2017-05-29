16078dfd0bf5b911706dfb5b38868ff7977d38c5
Chulle <philip_persson402@hotmail.com>
2017-05-29 13:35:33 +0200

    Frågorna

--- src/style/gui/CreatePDF.java
+++ src/style/gui/CreatePDF.java
@@ -1,10 +1,19 @@
 package style.gui;
 
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import style.gui.test.create.CreateNodes;
+import style.gui.test.take.TTest;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 /**
  * Created by Sofia on 2017-05-26.
@@ -26,6 +35,7 @@ public class CreatePDF {
         student = CreateNodes.createComboBox("Select student");
         button  = CreateNodes.createButton("Create");
 
+
         grid.add(headline,1,1);
         grid.add(test,1,2);
         grid.add(student,1,3);
@@ -38,4 +48,24 @@ public class CreatePDF {
     public GridPane getCreatePDF(){
     return grid;
     }
-}
+
+    public class PdfService {
+            public  void main(String[] args) {
+                Document document = new Document();
+
+
+                try {
+                    PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("testar.pdf"));
+                    document.open();
+                    document.add(new Paragraph());
+                    document.add(new Chunk(""));
+                    document.close();
+                    writer.close();
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
